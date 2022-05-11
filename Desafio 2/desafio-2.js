41190da9680d1a3f213c9559045e370870f2b3e4
nahuellegui12 <nahueljesusleguizamon@gmail.com>
2022-05-11 01:32:50 -0300

    Crea desafio 2

---
+++ Desafio 2/desafio-2.js
@@ -0,0 +1,18 @@
+//Calcular IVA de un producto. Si el precio es menor a $100 debe comprarlo, si es mayor no debe comprarlo
+
+let producto = prompt('Que producto queres comprar?');
+let precio= Number(prompt('Cuanto cuesta?'));
+
+function calcularPrecioIva(producto, precio){
+    return producto * 1.21;
+}
+
+console.log ("El producto " + producto + " Cuesta " + "$" + calcularPrecioIva(precio));
+
+precio = calcularPrecioIva(precio)
+
+if(precio < 100){
+    console.log("Comprar");
+} else {
+    console.log("No lo quiero")
+}
